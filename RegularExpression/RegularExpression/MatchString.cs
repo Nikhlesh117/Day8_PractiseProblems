fc7a5bbe71145a7e4fac912d104c7294fa4f949c
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-05-16 10:36:15 +0530

    Added: Match String

---
+++ RegularExpression/RegularExpression/MatchString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    public class MatchString
+    {
+        public void Match()
+        {
+            Console.WriteLine("Enter a string to match:");
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex("a[bc]{2,3}");
+            bool isMatch = regex.IsMatch(input);
+
+            if (isMatch)
+            {
+                Console.WriteLine("String matches the pattern.");
+            }
+            else
+            {
+                Console.WriteLine("String does not match the pattern.");
+            }
+        }
+    }
+}
---
+++ RegularExpression/RegularExpression/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    public  class Program
+    {
+        public static void Main(string[] args)
+        {
+            MatchString matchString = new MatchString();
+            matchString.Match();
+            Console.ReadLine();
+        }
+    }
+}
